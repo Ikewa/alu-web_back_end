55f4f3a69164ec8e984d6617c5fb522966e9093e
root <i.kewa@alustudent.com>
2025-01-10 17:30:41 +0000

    task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module demonstrates the use of type annotations in Python
+by assigning annotated variables with specific data types.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
