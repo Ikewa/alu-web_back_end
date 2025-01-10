6306ecd71a249c5ba170b10c96d815ece6f42117
root <i.kewa@alustudent.com>
2025-01-10 17:25:55 +0000

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to compute the floor of a float value.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Compute the floor of a float.
+
+    Args:
+        n (float): The float value to process.
+
+    Returns:
+        int: The largest integer less than or equal to the input float.
+    """
+    return math.floor(n)
